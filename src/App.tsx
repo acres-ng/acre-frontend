6f105d0880354637b94f4cc85bd069ca09ca1586
Abdulsalam Idris <ab4u66@gmail.com>
2023-09-29 00:50:22 +0100

    done with the design for login and signup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Login from "./components/login/Login";
+import Signup from "./components/signup/Signup";
+
+Login;
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
