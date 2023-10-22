c69ee7c30a724fe5110d2de58f55dad0c08579ea
Abdulsalam Idris <ab4u66@gmail.com>
2023-10-22 22:19:22 +0100

    Dashboard

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -160,7 +160,7 @@ export default function Dashboard() {
                               <Card>
                                 <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                                   <CardTitle className="text-sm font-medium">
-                                    Matorlity
+                                    Mortality
                                   </CardTitle>
                                   <svg
                                     xmlns="http://www.w3.org/2000/svg"
