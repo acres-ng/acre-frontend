8f45ebbde9a024e977dc03be8d5dd4182e23852d
Kiesem Senlong <36153131+kessysenlong@users.noreply.github.com>
2024-01-13 22:27:40 +0300

    init commit

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -13,7 +13,6 @@ import logo from "../../assets/logo.png";
 import useAuth from "../context/useAuth";
 import AuthContext from "../context/authContext";
 import { backgroundColours } from "@/lib/enums";
-import { fetchWeatherDataByGeocode } from "../dashboard/Dashboard";
 import { getActiveFarm } from "@/services/farmService";
 
 interface User {
