1421570e4058fd071b37074ad3261700aaea3b56
Lil Adonise <adenugaib24@gmail.com>
2024-02-29 13:25:31 +0100

    moving on with delete

--- src/components/livestock/table-footer.tsx
+++ src/components/livestock/table-footer.tsx
@@ -17,6 +17,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 interface TableFooterProps {
   checkedItems: string[];
@@ -33,47 +34,37 @@ export default function TableFooter({
   }
 
   return (
-    <div className="sticky bottom-0 left-0 z-10 mt-2.5 flex justify-end w-full items-center  rounded-md border border-gray-300 bg-white px-5 py-3.5 text-gray-900 shadow-sm dark:border-gray-300 dark:bg-gray-100 dark:text-white dark:active:bg-gray-100">
+    <div className="sticky bottom-0 left-0 z-10 mt-2.5 flex justify-end w-full items-center  ">
       <div>
-        <Text as="strong">{checkedItems.length}</Text> selected{" "}
         <Dialog>
-                <DialogTrigger asChild>
-        <Button
-          size="sm"
-          // variant="text"
-          className="underline"
-          color="danger"
-          // onClick={() => {
-          //   handleDelete(checkedItems);
-          // }}
-        >
-          Delete Them
-        </Button>
-        </DialogTrigger>
-        <DialogContent className=" rounded-2xl">
-                  <CardHeader>
-                    <CardTitle className="flex">
-                      <span className="mr-2 bg-red-200  border-b rounded-full p-2">
-                        <FaRegTrashAlt className="text-red-500" />
-                      </span>
-                      <span className="mt-2"> Delete Livestock</span>
-                    </CardTitle>
-                  </CardHeader>
-                  <CardContent>
-                    {/* <DialogTitle>Are you sure you want to permanently delete Livestock Name/ID from your farm?</DialogTitle> */}
-                    <DialogDescription className="font-medium text-black">
-                      Are you sure you want to permanently delete Livestock
-                      Name/ID from your farm?
-                    </DialogDescription>
-                  </CardContent>
-                  <CardFooter className="flex justify-between gap-4">
-                    <Button className="bg-white text-black shadow-md w-full">
-                      Cancel
-                    </Button>
-                    <Button className="bg-red-500 w-full">Delete</Button>
-                  </CardFooter>
-                </DialogContent>
-              </Dialog>
+          <DialogTrigger asChild>
+            <RiDeleteBin6Line className="text-red-500" />
+          </DialogTrigger>
+          <DialogContent className=" rounded-2xl">
+            <CardHeader>
+              <CardTitle className="flex">
+                <span className="mr-2 bg-red-200  border-b rounded-full p-2">
+                  <FaRegTrashAlt className="text-red-500" />
+                </span>
+                <span className="mt-2"> Delete Livestock</span>
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <Text as="strong">{checkedItems.length}</Text> selected{" "}
+              {/* <DialogTitle>Are you sure you want to permanently delete Livestock Name/ID from your farm?</DialogTitle> */}
+              <DialogDescription className="font-medium text-black">
+                Are you sure you want to permanently delete Livestock Name/ID
+                from your farm?
+              </DialogDescription>
+            </CardContent>
+            <CardFooter className="flex justify-between gap-4">
+              <Button className="bg-white text-black shadow-md w-full">
+                Cancel
+              </Button>
+              <Button className="bg-red-500 w-full">Delete</Button>
+            </CardFooter>
+          </DialogContent>
+        </Dialog>
       </div>
       {children}
     </div>
