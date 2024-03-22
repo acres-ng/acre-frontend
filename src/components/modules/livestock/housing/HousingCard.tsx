7f47219564eb873f665d6947b467a31d37320ea9
Lil Adonise <adenugaib24@gmail.com>
2024-03-23 00:26:51 +0100

    whewgit add . tricky action  modal but accomplished

--- src/components/modules/livestock/housing/HousingCard.tsx
+++ src/components/modules/livestock/housing/HousingCard.tsx
@@ -1,8 +1,39 @@
 import React from "react";
+import { useState } from 'react';
+import { BsThreeDotsVertical } from "react-icons/bs";
+// import { Popover } from "rizzui";
+import { Popover, Title, Text, Button } from "rizzui";
+import { ActionIcon } from "rizzui";
+import { AiOutlineTransaction } from "react-icons/ai";
+import { GiExpense } from "react-icons/gi";
+import {
+  Dialog,
+  DialogContent,
+  DialogClose,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from "../../../common/ui/dialog";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/common/ui/card";
+import { TbGrave2 } from "react-icons/tb";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { CiEdit } from "react-icons/ci";
+import { BiBowlRice } from "react-icons/bi";
+import { BiSolidBowlRice } from "react-icons/bi";
+import { PiDotsThreeOutlineVerticalFill } from "react-icons/pi";
+import { MdOutlineVaccines } from "react-icons/md";
+import { GoPulse } from "react-icons/go";
 
 // types
 type ImgAndIconProps = {
-      src: string;
+  src: string | React.ReactNode;
       alt: string;
       className?: string;
     };
@@ -28,8 +59,17 @@ type HousingCardProps = {
 
 // components
 const ImageWithAlt: React.FC<ImgAndIconProps> = ({ src, alt, className }) => (
-  <img loading="lazy" src={src} alt={alt} className={className}/>
+  <>
+    {typeof src === 'string' ? (
+      <img loading="lazy" src={src} alt={alt} className={className}/>
+    ) : (
+      <span className={className}>
+        {src === null ? null : src}
+      </span>
+    )}
+  </>
 );
+
 
 const Icon: React.FC<ImgAndIconProps> = ({ src, alt }) => (
   <ImageWithAlt src={src} alt={alt} className="w-6 aspect-square" />
@@ -39,6 +79,7 @@ const IconsSection: React.FC<IconsSectionProps>  = ({ icons }) => (
   <div className="flex gap-1 justify-between self-stretch">
     {icons.map((icon, index) => (
       <Icon key={index} src={icon.src} alt={icon.alt} />
+      
     ))}
   </div>
 );
@@ -56,7 +97,150 @@ const HousingInfo: React.FC<HousingInfoProps> = ({ name, totalCount, housingType
             alt: "Icon 1",
           },
           {
-            src: "https://cdn.builder.io/api/v1/image/assets/TEMP/6cf87a6d2b6316f8ece0787f73c87ed6cd39412d7a623f3a97c0fb39f8957552?apiKey=33b7de9dd450419096342a2b77215ab6&",
+            
+            src: (
+              <>
+          <Popover
+            placement="left"
+            className="z-50 min-w-[140px] px-2 dark:bg-gray-100 bg-white [&>svg]:dark:fill-gray-100"
+            content={() => (
+              <div className="text-gray-900">
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button
+                      variant="text"
+                      className="flex w-full items-center justify-start px-2 py-2 hover:bg-gray-100 focus:outline-none dark:hover:bg-gray-50"
+                    >
+                    
+                      <AiOutlineTransaction className="mr-2 h-5 w-5 text-gray-500" />
+                      Create Sales Order
+                    </Button>
+                  </DialogTrigger>
+
+                  <DialogContent className=" rounded-2xl">
+                    <CardHeader>
+                      <CardTitle className="flex">
+                        <span className="mr-2 bg-[#CCE6DA]  border-b rounded-full p-2">
+                          <AiOutlineTransaction className="text-green-500" />
+                        </span>
+                        <span className="mt-2"> Create Sales Order</span>
+                      </CardTitle>
+                    </CardHeader>
+
+                    <CardContent>
+                      {/* <SetFeedRation
+                        row={row}
+                        onRationCreated={handleRationCreated}
+                      /> */}
+                    </CardContent>
+                  </DialogContent>
+                </Dialog>
+
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button
+                      variant="text"
+                      className="flex w-full items-center justify-start px-2 py-2 hover:bg-gray-100 focus:outline-none dark:hover:bg-gray-50"
+                    >
+                      
+                      <TbGrave2 className="mr-2 h-5 w-5 text-gray-500" />
+                     Add mortality Record
+                    </Button>
+                  </DialogTrigger>
+
+                  <DialogContent className=" rounded-2xl">
+                    <CardHeader>
+                      <CardTitle className="flex">
+                        <span className="mr-2 bg-[#CCE6DA]  border-b rounded-full p-2">
+                          <TbGrave2 className="text-green-500" />
+                        </span>
+                        <span className="mt-2">Add mortality Record</span>
+                      </CardTitle>
+                    </CardHeader>
+
+                    <CardContent>
+                      {/* <SetFeedRation /> */}
+                    </CardContent>
+                  </DialogContent>
+                </Dialog>
+
+
+
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button
+                      variant="text"
+                      className="flex w-full items-center justify-start px-2 py-2 hover:bg-gray-100 focus:outline-none dark:hover:bg-gray-50"
+                    >
+                      
+                      <MdOutlineVaccines className="mr-2 h-5 w-5 text-gray-500" />
+                      Add Vaccination Record
+                    </Button>
+                  </DialogTrigger>
+
+                  <DialogContent className=" rounded-2xl">
+                    <CardHeader>
+                      <CardTitle className="flex">
+                        <span className="mr-2 bg-[#CCE6DA]  border-b rounded-full p-2">
+                          <GoPulse className="text-green-500" />
+                        </span>
+                        <span className="mt-2">Add Vaccination Record</span>
+                      </CardTitle>
+                    </CardHeader>
+
+                    <CardContent>
+                      {/* <SetFeedRation
+                        row={row}
+                        onRationCreated={handleRationCreated}
+                      /> */}
+                    </CardContent>
+                  </DialogContent>
+                </Dialog>
+
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button
+                      variant="text"
+                      className="flex w-full items-center justify-start px-2 py-2 hover:bg-gray-100 focus:outline-none dark:hover:bg-gray-50"
+                    >
+                    
+
+                      <GiExpense className="mr-2 h-5 w-5 text-gray-500" />
+                      Add general expense record
+                    </Button>
+                  </DialogTrigger>
+
+                  <DialogContent className=" rounded-2xl">
+                    <CardHeader>
+                      <CardTitle className="flex">
+                        <span className="mr-2 bg-[#CCE6DA]  border-b rounded-full p-2">
+                          <BiSolidBowlRice className="text-green-500" />
+                        </span>
+                        <span className="mt-2"> Add expense record</span>
+                      </CardTitle>
+                    </CardHeader>
+
+                    <CardContent>
+                      {/* <SetFeedRation
+                        row={row}
+                        onRationCreated={handleRationCreated}
+                      /> */}
+                    </CardContent>
+                  </DialogContent>
+                </Dialog>
+
+
+
+              </div>
+            )}
+          >
+            <ActionIcon variant="text">
+              <PiDotsThreeOutlineVerticalFill className="h-[18px] w-[18px] text-gray-500" />
+            </ActionIcon>
+          </Popover>
+              </>
+            
+            ),
             alt: "Icon 2",
           },
         ]}
@@ -81,6 +265,7 @@ const HousingInfo: React.FC<HousingInfoProps> = ({ name, totalCount, housingType
 );
 
 const HousingCard: React.FC<HousingCardProps> = ({imageSrc, imageAlt, info}) => (
+  
   <div className="flex gap-5 max-md:flex-col max-md:gap-0 max-md: mb-5">
     <aside className="flex flex-col w-[27%] max-md:ml-0 max-md:w-full">
       <ImageWithAlt
